d1aefe7365f4ba514fb9e5f2c97a19451f1f1ad1
AniModi <98632268+AniModi@users.noreply.github.com>
2023-07-12 15:59:07 +0530

    added connect button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,123 @@
-import React, { useState } from 'react';
-import '../assets/styles/components/Navbar.scss';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "../assets/styles/components/Navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [menu, setMenu] = useState("navbar_container__menu__hide");
-    const handleMenu = () => {
-        if(menu === "navbar_container__menu__hide")
-            setMenu("navbar_container__menu");
-        else
-            setMenu("navbar_container__menu__hide");
-        console.log("Menu clicked");
+  const [menu, setMenu] = useState("navbar_container__menu__hide");
+  const [isConnected, setIsConnected] = useState(false);
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum
+        .enable()
+        .then(() => {
+          setIsConnected(true);
+        })
+        .catch(() => {
+          setIsConnected(false);
+        });
+    } else {
+      setIsConnected(false);
     }
-    return (
-        <>
-            <div className="navbar_container">
-                <div className="navbar_container__left" onClick={handleMenu}>
-                    Menu 
-                </div>
-                <div className="navbar_container__right">
-                    <div className="navbar_container__right__circle">
-                        <div className="navbar_container__right__circle__img">
-                        </div>
-                    </div>
-                </div>
-                <div className= {menu}>
-                    <div className="navbar_container__menu__content">
-                        <Link className="navbar_container__menu__content__item" to={"/my-nfts"} onClick={handleMenu}>My NFTs</Link>
-                        <Link className="navbar_container__menu__content__item" to={"/add-nft"} onClick={handleMenu}>Add NFT</Link>
-                        <Link className="navbar_container__menu__content__item" to={"/buy-house"} onClick={handleMenu}>Buy House</Link>
-                        <Link className="navbar_container__menu__content__item" to={"/sell-house"} onClick={handleMenu}>Sell House</Link>
-                        <Link className="navbar_container__menu__content__item" to={"/buy-land"} onClick={handleMenu}>Buy Land</Link>
-                        <Link className="navbar_container__menu__content__item" to={"/sell-land"} onClick={handleMenu}>Sell Land</Link>
-                        <Link className="navbar_container__menu__content__item" to={"/lend"} onClick={handleMenu}>Lend Money</Link>
-                        <Link className="navbar_container__menu__content__item" to={"/borrow"} onClick={handleMenu}>Borrow Money</Link>
-                        <Link className="navbar_container__menu__content__item" to={"/my-profile"} onClick={handleMenu}>My Profile</Link>
-                    </div>
-                </div>
+  }, [isConnected]);
+  const connect = () => {
+    console.log("Connect clicked");
+  };
+  const handleMenu = () => {
+    if (menu === "navbar_container__menu__hide")
+      setMenu("navbar_container__menu");
+    else setMenu("navbar_container__menu__hide");
+    console.log("Menu clicked");
+  };
+  return (
+    <>
+      <div className="navbar_container">
+        <div className="navbar_container__left" onClick={handleMenu}>
+          Menu
+        </div>
+        <div className="navbar_container__right">
+          {isConnected ? (
+            <div className="navbar_container__right__connected">
+              Connected!
             </div>
-        </>
-    );
-}
+          ) : (
+            <div
+              className="navbar_container__right__disconnected"
+              onClick={connect}
+            >
+              Connect
+            </div>
+          )}
+        </div>
+        <div className={menu}>
+          <div className="navbar_container__menu__content">
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/my-nfts"}
+              onClick={handleMenu}
+            >
+              My NFTs
+            </Link>
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/add-nft"}
+              onClick={handleMenu}
+            >
+              Add NFT
+            </Link>
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/buy-house"}
+              onClick={handleMenu}
+            >
+              Buy House
+            </Link>
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/sell-house"}
+              onClick={handleMenu}
+            >
+              Sell House
+            </Link>
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/buy-land"}
+              onClick={handleMenu}
+            >
+              Buy Land
+            </Link>
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/sell-land"}
+              onClick={handleMenu}
+            >
+              Sell Land
+            </Link>
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/lend"}
+              onClick={handleMenu}
+            >
+              Lend Money
+            </Link>
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/borrow"}
+              onClick={handleMenu}
+            >
+              Borrow Money
+            </Link>
+            <Link
+              className="navbar_container__menu__content__item"
+              to={"/my-profile"}
+              onClick={handleMenu}
+            >
+              My Profile
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
