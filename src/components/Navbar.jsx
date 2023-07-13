1fbb6261960418d0f409bdb23bdd1dd035d52122
Rachit Chahar <202151123@iiitvadodara.ac.in>
2023-07-13 12:39:13 +0530

    add nft

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../assets/styles/components/Navbar.scss";
 import { Link } from "react-router-dom";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
--- src/containers/SellHouseForm.jsx
+++ src/containers/SellHouseForm.jsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import "../assets/styles/containers/SellHouseForm.scss";
 import Navbar from '../components/Navbar';
 import InputBox from '../components/InputBox';
+import { PnftAddress, PnftAbi, PdealAddress, PdealAbi } from '../constants';
+import { writeContract, readContract } from 'wagmi/actions';
+import lighthouse from '@lighthouse-web3/sdk';
+import { Polybase } from "@polybase/client";
 
+const db = new Polybase({
+  defaultNamespace: "pk/0x1a57dc69d2e8e6938a05bdefbebd62622ddbb64038f7347bd4fe8beb37b9bf40d5e8b62eaf9de36cbff52904b7f81bff22b29716021aaa8c11ee552112143259/CB",
+});
+const db_metadata = db.collection('PropertyNFTMetadata')
+
+const apiKey = 'a479e403.cac0fbe03f9b43cf8875c512a8321308'
 
 const SellHouseForm = () => {
-    const form = [
+     const form = [
         {
             label: "Property age",
             name: "age",
@@ -16,7 +26,7 @@ const SellHouseForm = () => {
         {
             label: "Property type",
             name: "type",
-            placeholder: "Apartment | Villa | Independent House",
+            placeholder: "Apartment | Villa | Independent House | Land",
             type: "text",
             value: ""
         },
@@ -28,6 +38,13 @@ const SellHouseForm = () => {
             value: ""
         },
         {
+            label: "Property description",
+            name: "desc",
+            placeholder: "No. of rooms, bathrooms, swimming pool, etc.",
+            type: "text",
+            value: ""
+        },
+        {
             label: "Address",
             name: "address",
             placeholder: "Address",
@@ -35,7 +52,7 @@ const SellHouseForm = () => {
             value: ""
         },
         {
-            label: "City",
+            label: "Location",
             name: "city",
             placeholder: "City, State",
             type: "text",
@@ -49,21 +66,21 @@ const SellHouseForm = () => {
             value: ""
         },
         {
-            label: "Google map link",
+            label: "Google maps link",
             name: "link",
-            placeholder: "Google map link",
+            placeholder: "Google maps link",
             type: "text",
             value: ""
         },
         {
-            label: "Price",
+            label: "Estimated value",
             name: "price",
-            placeholder: "Price",
+            placeholder: "Estimated value",
             type: "number",
             value: ""
         },
         {
-            label: "Images",
+            label: "Property Images",
             name: "images",
             placeholder: "Images",
             type: "file",
@@ -77,11 +94,12 @@ const SellHouseForm = () => {
             value: ""
         },
     ];
-
-    const [formState, setFormState] = useState({
+    
+    const defaultFormState = {
         age: "",
         type: "",
         size: "",
+        desc: "",
         address: "",
         city: "",
         pincode: "",
@@ -89,14 +107,16 @@ const SellHouseForm = () => {
         images: "",
         title_deed: "",
         link: ""
-    });
+    }
+    const [formState, setFormState] = useState(defaultFormState);
 
-    const handleInput = (event) => {
+    const handleInput = async(event) => {
         if(event.target.type === "file") {
             setFormState({
                 ...formState,
                 [event.target.name]: event.target.files
             });
+            console.log(event.target.files);
             return;
         }
         setFormState({
@@ -104,9 +124,75 @@ const SellHouseForm = () => {
             [event.target.name]: event.target.value
         });
     };
-    const handleSubmit = () => {
-        console.log(formState);
+    const progressCallback = (progressData) => {
+        let percentageDone =
+          100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
+        console.log(percentageDone);
+      };
+
+    const uploadImages = async (images) => {
+        if(images.length === 0) return;
+        let _images;
+        let links = [];
+
+        for(let i=0; i<images.length; i++) {
+            const file = images[i];
+            _images = [file];
+            const response = await lighthouse.upload(_images, apiKey, progressCallback);
+            console.log('Visit at https://gateway.lighthouse.storage/ipfs/' + response.data.Hash)
+            const link = 'https://gateway.lighthouse.storage/ipfs/' + response.data.Hash;
+            links.push(link);
+        }
+        return links;
+    }
+
+    const uploadTitleDeed = async (title_deed) => {
+        if(title_deed.length === 0) return;
+        const response = await lighthouse.upload(title_deed, apiKey, progressCallback);
+        console.log('Visit at https://gateway.lighthouse.storage/ipfs/' + response.data.Hash)
+        const link = 'https://gateway.lighthouse.storage/ipfs/' + response.data.Hash;
+        return link;
+    }
+
+    const handleSubmit = async () => {
+        const imagesLink = await uploadImages(formState.images);
+        const titleDeedLink = await uploadTitleDeed(formState.title_deed);
+        const totalSupply = await readContract({
+            address: PnftAddress,
+            abi: PnftAbi,
+            functionName: "totalSupply",
+        });
+        const recordId = (parseInt(totalSupply.toString()) +1).toString();
+        const recordData = await db_metadata.create([
+            recordId,
+            'Property NFT ',
+            imagesLink[0],
+            formState.age,
+            formState.type,
+            formState.size,
+            formState.desc,
+            formState.address,
+            formState.city,
+            formState.pincode,
+            formState.link,
+            formState.price,
+            imagesLink,
+            titleDeedLink
+        ]);
+
+ const url = `https://testnet.polybase.xyz/v0/collections/pk%2F0x1a57dc69d2e8e6938a05bdefbebd62622ddbb64038f7347bd4fe8beb37b9bf40d5e8b62eaf9de36cbff52904b7f81bff22b29716021aaa8c11ee552112143259%2FCB%2FPropertyNFTMetadata/records/${recordId}`+'?format=nft`';
+        console.log('visit at --> ', url)
+
+        const tokenId = await writeContract({  
+            address: PnftAddress,
+            abi: PnftAbi,
+            functionName: "mint",
+            args: [url],
+        });
+        console.log(tokenId);
+        setFormState(defaultFormState);
     };
+
     return(
         <>
         <Navbar></Navbar>
